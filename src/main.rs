81f57ecfe18ddc67162b78786400a4cd54cb8282
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2023-05-02 20:43:33 +0100

    comment

--- src/main.rs
+++ src/main.rs
@@ -32,103 +32,103 @@ impl Greeter for MyGreeter {
         println!("request: {:?}", request);
         let inner = request.into_inner();
 
-        // https://stripe.com/docs/payments/save-and-reuse
+        // // https://stripe.com/docs/payments/save-and-reuse
 
-        // Create `Customer`
-        // https://stripe.com/docs/payments/save-and-reuse?platform=web#create-customer
-        // https://stripe.com/docs/api/customers/create
-        let customer_id = {
-            let mut json = serde_json::to_value(&inner.customer).unwrap();
-            let req = hyper::Request::post("https://api.stripe.com/v1/customers")
-                .header("user", format!("{STRIPE_SECRET}:"))
-                .body(hyper::Body::from(json.to_string()))
-                .unwrap();
-            let res = self.stripe_client.request(req).await.unwrap();
-            let body = res.collect().await.unwrap().aggregate();
-            let customer: serde_json::Value = serde_json::from_reader(body.reader()).unwrap();
-            let customer_id = customer
-                .as_object()
-                .unwrap()
-                .get("id")
-                .unwrap()
-                .as_str()
-                .unwrap();
-            customer_id
-        };
-        println!("customer_id: {customer_id}");
+        // // Create `Customer`
+        // // https://stripe.com/docs/payments/save-and-reuse?platform=web#create-customer
+        // // https://stripe.com/docs/api/customers/create
+        // let customer_id = {
+        //     let mut json = serde_json::to_value(&inner.customer).unwrap();
+        //     let req = hyper::Request::post("https://api.stripe.com/v1/customers")
+        //         .header("user", format!("{STRIPE_SECRET}:"))
+        //         .body(hyper::Body::from(json.to_string()))
+        //         .unwrap();
+        //     let res = self.stripe_client.request(req).await.unwrap();
+        //     let body = res.collect().await.unwrap().aggregate();
+        //     let customer: serde_json::Value = serde_json::from_reader(body.reader()).unwrap();
+        //     let customer_id = customer
+        //         .as_object()
+        //         .unwrap()
+        //         .get("id")
+        //         .unwrap()
+        //         .as_str()
+        //         .unwrap();
+        //     customer_id
+        // };
+        // println!("customer_id: {customer_id}");
 
-        // Create card
-        // https://stripe.com/docs/api/cards/create
-        let payment_id = {
-            let mut json = serde_json::to_value(&inner.card).unwrap();
-            json.as_object_mut()
-                .unwrap()
-                .insert(
-                    String::from("object"),
-                    serde_json::Value::String(String::from("card")),
-                )
-                .unwrap();
-            let source = serde_json::json!({ "source": json });
-            let req = hyper::Request::post(format!(
-                "https://api.stripe.com/v1/customers/{customer_id}/sources"
-            ))
-            .header("user", format!("{STRIPE_SECRET}:"))
-            .body(hyper::Body::from(source.to_string()))
-            .unwrap();
-            let res = self.stripe_client.request(req).await.unwrap();
-            let body = res.collect().await.unwrap().aggregate();
-            let payment: serde_json::Value = serde_json::from_reader(body.reader()).unwrap();
-            let payment_id = payment
-                .as_object()
-                .unwrap()
-                .get("id")
-                .unwrap()
-                .as_str()
-                .unwrap();
-            payment_id
-        };
-        println!("payment_id: {payment_id}");
+        // // Create card
+        // // https://stripe.com/docs/api/cards/create
+        // let payment_id = {
+        //     let mut json = serde_json::to_value(&inner.card).unwrap();
+        //     json.as_object_mut()
+        //         .unwrap()
+        //         .insert(
+        //             String::from("object"),
+        //             serde_json::Value::String(String::from("card")),
+        //         )
+        //         .unwrap();
+        //     let source = serde_json::json!({ "source": json });
+        //     let req = hyper::Request::post(format!(
+        //         "https://api.stripe.com/v1/customers/{customer_id}/sources"
+        //     ))
+        //     .header("user", format!("{STRIPE_SECRET}:"))
+        //     .body(hyper::Body::from(source.to_string()))
+        //     .unwrap();
+        //     let res = self.stripe_client.request(req).await.unwrap();
+        //     let body = res.collect().await.unwrap().aggregate();
+        //     let payment: serde_json::Value = serde_json::from_reader(body.reader()).unwrap();
+        //     let payment_id = payment
+        //         .as_object()
+        //         .unwrap()
+        //         .get("id")
+        //         .unwrap()
+        //         .as_str()
+        //         .unwrap();
+        //     payment_id
+        // };
+        // println!("payment_id: {payment_id}");
 
-        // Create `SetupIntent`
-        // https://stripe.com/docs/payments/save-and-reuse?platform=web#web-create-intent
-        // https://stripe.com/docs/api/setup_intents/create
-        {
-            let body = serde_json::json! ({
-                "confirm": true,
-                "customer": customer_id,
-                "payment_method": payment_id,
-                "payment_method_types": ["card"]
-            });
-            let req = hyper::Request::builder()
-                .header("user", format!("{STRIPE_SECRET}:"))
-                .body(hyper::Body::from(body.to_string()))
-                .unwrap();
-            let res = self.stripe_client.request(req).await.unwrap();
-            let body = res.collect().await.unwrap().aggregate();
-            let setup_intent: serde_json::Value = serde_json::from_reader(body.reader()).unwrap();
-            println!("setup_intent: {setup_intent}");
-        }
+        // // Create `SetupIntent`
+        // // https://stripe.com/docs/payments/save-and-reuse?platform=web#web-create-intent
+        // // https://stripe.com/docs/api/setup_intents/create
+        // {
+        //     let body = serde_json::json! ({
+        //         "confirm": true,
+        //         "customer": customer_id,
+        //         "payment_method": payment_id,
+        //         "payment_method_types": ["card"]
+        //     });
+        //     let req = hyper::Request::builder()
+        //         .header("user", format!("{STRIPE_SECRET}:"))
+        //         .body(hyper::Body::from(body.to_string()))
+        //         .unwrap();
+        //     let res = self.stripe_client.request(req).await.unwrap();
+        //     let body = res.collect().await.unwrap().aggregate();
+        //     let setup_intent: serde_json::Value = serde_json::from_reader(body.reader()).unwrap();
+        //     println!("setup_intent: {setup_intent}");
+        // }
 
-        // Create test payment
-        {
+        // // Create test payment
+        // {
             
-        }
+        // }
         
         let reply = hello_world::HelloReply {
-            hello: format!("Hello {}", request.get_ref().name),
+            hello: format!("Hello {}", inner.name),
         };
 
         Ok(Response::new(reply)) // Send back our formatted greeting
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct User {
-    hash: String,
-    // TODO Use `SocketAddrV6` or a stricter type
-    repo: Option<String>,
-    instance: Option<String>,
-}
+// #[derive(Debug, Serialize, Deserialize)]
+// struct User {
+//     hash: String,
+//     // TODO Use `SocketAddrV6` or a stricter type
+//     repo: Option<String>,
+//     instance: Option<String>,
+// }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -151,6 +151,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         stripe_client: client,
     };
     let addr = SocketAddr::V6(SocketAddrV6::new(address, port, 0, 0));
+
+    println!("addr: {addr}");
 
     Server::builder()
         .add_service(GreeterServer::new(greeter))
